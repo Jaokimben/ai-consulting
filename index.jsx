764696dec5c928cb8546ad6e68df1d99296bfc84
Jaokimben <joakimben1234@gmail.com>
2025-05-10 19:28:52 +0200

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar1 } from "./components/Navbar1";
+import { Footer3 } from "./components/Footer3";
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar1 />
+      <Footer3 />
+    </div>
+  );
+}
